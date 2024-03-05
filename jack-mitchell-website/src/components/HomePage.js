a67697c4cd76cd938c27fafd41d8957f8baa3d1b
Jack Mitchell <mitchell.jack@northeastern.edu>
2024-03-05 17:23:06 -0500

    Fixed

--- jack-mitchell-website/src/components/HomePage.js
+++ jack-mitchell-website/src/components/HomePage.js
@@ -1,29 +1,22 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import '../Homepage.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHandshake } from '@fortawesome/free-solid-svg-icons';
 import rowing_img from '../Rowing_Sunrise.jpg';
 
-
 const HomePage = () => {
- return (
-   <Container fluid className="my-jumbotron my-5 p-5 text-white" style={{ backgroundImage: {rowing_img}, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-     <Row className="align-items-center" style={{ minHeight: '80vh' }}>
-       <Col className="text-center">
-         <h1 className="display-4 fw-bold">Welcome to My Personal Website</h1>
-         <p className="lead">
-         <FontAwesomeIcon icon={faHandshake} /> Nice to meet you!
-         </p>
-         <Button variant="light" className="mt-3" as="a" href="/about">Explore More</Button>
-       </Col>
-     </Row>
-   </Container>
- );
+  return (
+    <Container fluid className="my-jumbotron my-5 p-5 text-white" style={{ backgroundImage: `url(${rowing_img})`, backgroundSize: 'cover', backgroundPosition: 'center', borderRadius: '15px' }}>
+      <Row className="align-items-center" style={{ minHeight: '80vh' }}>
+        <Col className="text-center">
+          <h1 className="display-4 fw-bold">Welcome to My Personal Website</h1>
+          <p className="lead">
+            Nice to meet you!
+          </p>
+          <Button variant="light" className="mt-3" as="a" href="/about">Learn More</Button>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
-
 export default HomePage;
-
-
-
